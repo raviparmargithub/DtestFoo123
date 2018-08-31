5a53b12bfc5022644ebeee016e1d465b4d5c8940
Ravi Parmar <ravi.parmar@zebpay.com>
2018-08-31 15:07:12 +0530

    commit

---
+++ DtestFoo123/MyFramework.swift
@@ -0,0 +1,14 @@
+//
+//  MyFramework.swift
+//  DtestFoo123
+//
+//  Created by Ravi Parmar on 31/08/18.
+//  Copyright © 2018 Ravi Parmar. All rights reserved.
+//
+
+public class MyFramework {
+    static let shared = MyFramework ()
+    func foo() {
+        print("Ravi")
+    }
+}
